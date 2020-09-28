6b67c5191cdea727017031ed5f5ed34428c83eba
Abdullah <abogutalan35@gmail.com>
2020-09-28 13:06:10 -0400

    read from txt

--- server.c
+++ server.c
@@ -15,9 +15,9 @@
 #include <sys/wait.h>
 #include <signal.h>
 
-#define PORT "29732"  // the port users will be connecting to
+#define PORT "29732" // the port users will be connecting to
 
-#define BACKLOG 10	 // how many pending connections queue will hold
+#define BACKLOG 10 // how many pending connections queue will hold
 
 void sigchld_handler(int s)
 {
@@ -26,30 +26,75 @@ void sigchld_handler(int s)
 	// waitpid() might overwrite errno, so we save and restore it:
 	int saved_errno = errno;
 
-	while(waitpid(-1, NULL, WNOHANG) > 0);
+	while (waitpid(-1, NULL, WNOHANG) > 0)
+		;
 
 	errno = saved_errno;
 }
 
-
 // get sockaddr, IPv4 or IPv6:
 void *get_in_addr(struct sockaddr *sa)
 {
-	if (sa->sa_family == AF_INET) {
-		return &(((struct sockaddr_in*)sa)->sin_addr);
+	if (sa->sa_family == AF_INET)
+	{
+		return &(((struct sockaddr_in *)sa)->sin_addr);
 	}
 
-	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+	return &(((struct sockaddr_in6 *)sa)->sin6_addr);
+}
+
+// https://stackoverflow.com/questions/28677004/how-to-assign-text-file-data-to-variable
+char *readFileContent(const char *const filename)
+{
+	size_t size;
+	FILE *file;
+	char *data;
+
+	file = fopen(filename, "r");
+	if (file == NULL)
+	{
+		perror("fopen()\n");
+		return NULL;
+	}
+
+	/* get the file size by seeking to the end and getting the position */
+	fseek(file, 0L, SEEK_END);
+	size = ftell(file);
+	/* reset the file position to the begining. */
+	rewind(file);
+
+	/* allocate space to hold the file content */
+	data = malloc(1 + size);
+	if (data == NULL)
+	{
+		perror("malloc()\n");
+		fclose(file);
+		return NULL;
+	}
+	/* nul terminate the content to make it a valid string */
+	data[size] = '\0';
+	/* attempt to read all the data */
+	if (fread(data, 1, size, file) != size)
+	{
+		perror("fread()\n");
+
+		free(data);
+		fclose(file);
+
+		return NULL;
+	}
+	fclose(file);
+	return data;
 }
 
 int main(void)
 {
-	int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+	int sockfd, new_fd; // listen on sock_fd, new connection on new_fd
 	struct addrinfo hints, *servinfo, *p;
 	struct sockaddr_storage their_addr; // connector's address information
 	socklen_t sin_size;
 	struct sigaction sa;
-	int yes=1;
+	int yes = 1;
 	char s[INET6_ADDRSTRLEN];
 	int rv;
 
@@ -58,26 +103,31 @@ int main(void)
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE; // use my IP
 
-	if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+	if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0)
+	{
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
 		return 1;
 	}
 
 	// loop through all the results and bind to the first we can
-	for(p = servinfo; p != NULL; p = p->ai_next) {
+	for (p = servinfo; p != NULL; p = p->ai_next)
+	{
 		if ((sockfd = socket(p->ai_family, p->ai_socktype,
-				p->ai_protocol)) == -1) {
+							 p->ai_protocol)) == -1)
+		{
 			perror("server: socket");
 			continue;
 		}
 
 		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
-				sizeof(int)) == -1) {
+					   sizeof(int)) == -1)
+		{
 			perror("setsockopt");
 			exit(1);
 		}
 
-		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1)
+		{
 			close(sockfd);
 			perror("server: bind");
 			continue;
@@ -88,12 +138,14 @@ int main(void)
 
 	freeaddrinfo(servinfo); // all done with this structure
 
-	if (p == NULL)  {
+	if (p == NULL)
+	{
 		fprintf(stderr, "server: failed to bind\n");
 		exit(1);
 	}
 
-	if (listen(sockfd, BACKLOG) == -1) {
+	if (listen(sockfd, BACKLOG) == -1)
+	{
 		perror("listen");
 		exit(1);
 	}
@@ -101,34 +153,52 @@ int main(void)
 	sa.sa_handler = sigchld_handler; // reap all dead processes
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = SA_RESTART;
-	if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+	if (sigaction(SIGCHLD, &sa, NULL) == -1)
+	{
 		perror("sigaction");
 		exit(1);
 	}
 
 	printf("server: waiting for connections...\n");
 
-	while(1) {  // main accept() loop
+	while (1)
+	{ // main accept() loop
 		sin_size = sizeof their_addr;
 		new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-		if (new_fd == -1) {
+		if (new_fd == -1)
+		{
 			perror("accept");
 			continue;
 		}
 
 		inet_ntop(their_addr.ss_family,
-			get_in_addr((struct sockaddr *)&their_addr),
-			s, sizeof s);
+				  get_in_addr((struct sockaddr *)&their_addr),
+				  s, sizeof s);
 		printf("server: got connection from %s\n", s);
 
-		if (!fork()) { // this is the child process
+		if (!fork())
+		{				   // this is the child process
 			close(sockfd); // child doesn't need the listener
-			if (send(new_fd, "Hello, world!", 13, 0) == -1)
-				perror("send");
-			close(new_fd);
-			exit(0);
+
+			// file read
+			char *content;
+			content = readFileContent("test.txt");
+			if (content != NULL)
+			{
+				printf("%s\n", content);
+
+				// getting file size of the txt file
+				int length = strlen(content);
+				printf("length of the file: %d", length);
+				if (send(new_fd, content, length, 0) == -1)
+					perror("send");
+				free(content);
+				close(new_fd);
+				exit(0);
+			}
+
 		}
-		close(new_fd);  // parent doesn't need this
+		close(new_fd); // parent doesn't need this
 	}
 
 	return 0;
